bb435c07cc4aede4944bc2c4d6676dd0e93e8687
Jan-Paul Kleemans <jpkleemans@gmail.com>
2022-10-06 09:36:34 +0200

    Cleanup

--- src/PrintOneServiceProvider.php
+++ src/PrintOneServiceProvider.php
@@ -10,16 +10,8 @@ class PrintOneServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('print-one')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_print-one_table')
-            ->hasCommand(PrintOneCommand::class);
     }
 }
--- tests/Pest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Nexxtbi\PrintOne\Tests\TestCase;
-
-uses(TestCase::class)->in(__DIR__);
