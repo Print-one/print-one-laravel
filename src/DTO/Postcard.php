87813923a3c9ed569fe73446c0d3e1d13ae2e9cd
Bram Wubs <bram@sibi.nl>
2022-10-11 11:49:43 +0200

    Introduce Postcard dto to combine front and back templates into one concept

---
+++ src/DTO/Postcard.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Nexibi\PrintOne\DTO;
+
+class Postcard
+{
+    public string $format;
+
+    public function __construct(public Template $front, public Template $back)
+    {
+        if ($this->front->format !== $this->back->format) {
+            throw new \InvalidArgumentException("Front and back template should be of the same format");
+        }
+        $this->format = $this->front->format;
+    }
+}
