430c51c4aa926b8f60440fc17d1400a8a8107959
Bram Wubs <bram@sibi.nl>
2022-10-12 11:32:35 +0200

    Arrayable stuff

--- src/DTO/Postcard.php
+++ src/DTO/Postcard.php
@@ -2,9 +2,31 @@
 
 namespace Nexibi\PrintOne\DTO;
 
-class Postcard
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Jsonable;
+
+class Postcard implements Arrayable, Jsonable
 {
-    public function __construct(public string $front, public string $back, public string $format)
+    public function __construct(public string $front, public string $back, public string $format) {}
+
+    public static function fromArray(array $data): self
     {
+        return new self(
+            $data['front'], $data['back'], $data['format']
+        );
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'front' => $this->front,
+            'back' => $this->back,
+            'format' => $this->format,
+        ];
+    }
+
+    public function toJson($options = 0): string
+    {
+        return json_encode($this->toArray(), $options);
     }
 }
